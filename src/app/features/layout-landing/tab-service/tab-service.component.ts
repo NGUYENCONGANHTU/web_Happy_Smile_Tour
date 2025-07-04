d4c60904fc48c55148f5c37f8010019d13af8765
RoBin <108477315+NGUYENCONGANHTU@users.noreply.github.com>
2025-07-04 16:40:59 +0700

    feat(ui): tab service

--- src/app/features/layout-landing/tab-service/tab-service.component.ts
+++ src/app/features/layout-landing/tab-service/tab-service.component.ts
@@ -1,8 +1,7 @@
-import {Component, inject, OnInit} from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { faPhoneFlip } from '@fortawesome/free-solid-svg-icons';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { NzColDirective, NzRowDirective } from 'ng-zorro-antd/grid';
-import { NzDatePickerComponent } from 'ng-zorro-antd/date-picker';
 import {
   NzFormControlComponent,
   NzFormDirective,
@@ -11,14 +10,12 @@ import {
 } from 'ng-zorro-antd/form';
 import { NzInputDirective } from 'ng-zorro-antd/input';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import {AppService} from '../../../../app.service';
 @Component({
   selector: 'app-tab-service',
   standalone: true,
   imports: [
     FaIconComponent,
     NzColDirective,
-    NzDatePickerComponent,
     NzFormControlComponent,
     NzFormDirective,
     NzFormItemComponent,
@@ -30,12 +27,8 @@ import {AppService} from '../../../../app.service';
   templateUrl: './tab-service.component.html',
   styleUrl: './tab-service.component.scss',
 })
-export class TabServiceComponent implements  OnInit {
-  appService = inject(AppService);
-  ngOnInit() {
-  }
-
-
+export class TabServiceComponent {
+  // ============================================================================================== //
   faPhoneFlip = faPhoneFlip;
   private fb = inject(FormBuilder);
   validateForm = this.fb.group({
