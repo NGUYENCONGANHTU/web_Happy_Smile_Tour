9b8d45c6079f75804b43dca6db1b979d8af33b19
RoBin <108477315+NGUYENCONGANHTU@users.noreply.github.com>
2025-07-06 19:11:29 +0700

    feat(ui): ghep api and fix UI tab service

--- src/app/features/layout-landing/tab-service/tab-service.component.ts
+++ src/app/features/layout-landing/tab-service/tab-service.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { faPhoneFlip } from '@fortawesome/free-solid-svg-icons';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { NzColDirective, NzRowDirective } from 'ng-zorro-antd/grid';
@@ -10,6 +10,9 @@ import {
 } from 'ng-zorro-antd/form';
 import { NzInputDirective } from 'ng-zorro-antd/input';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AppService } from '../../../../app.service';
+import { ContactVisaReqDTO, PrivateTourResDTO } from '../../../../interface';
+import { NgStyle } from '@angular/common';
 @Component({
   selector: 'app-tab-service',
   standalone: true,
@@ -23,23 +26,80 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
     NzRowDirective,
     ReactiveFormsModule,
     NzFormLabelComponent,
+    NgStyle,
   ],
   templateUrl: './tab-service.component.html',
   styleUrl: './tab-service.component.scss',
 })
-export class TabServiceComponent {
-  // ============================================================================================== //
+export class TabServiceComponent implements OnInit {
   faPhoneFlip = faPhoneFlip;
+  appService = inject(AppService);
+
+  ngOnInit() {
+    this.getDataBannerTabService();
+    this.getDataVisaTabService();
+    this.getDataContentTabService();
+    this.getDataTitleWorkflowTabService();
+    this.getDataWorkflowTabService();
+  }
+
+  // get data Banner tab service
+  dataBannerTabService: PrivateTourResDTO[] = [];
+  getDataBannerTabService() {
+    this.appService.getAllDataBannerTabService().subscribe(data => {
+      this.dataBannerTabService = data;
+    });
+  }
+  // get data VISA tab service
+  dataVisaTabService: PrivateTourResDTO[] = [];
+  getDataVisaTabService() {
+    this.appService.getAllDataVisaPrivateTour().subscribe(data => {
+      this.dataVisaTabService = data;
+    });
+  }
+  // get data Content tab service
+  dataContentTabService: PrivateTourResDTO[] = [];
+  getDataContentTabService() {
+    this.appService.getAllDataContentTabService().subscribe(data => {
+      this.dataContentTabService = data;
+    });
+  }
+  // Tiêu đề quy trình
+  dataTitleWorkflowTabService: PrivateTourResDTO[] = [];
+  getDataWorkflowTabService() {
+    this.appService.getAllDataWorkflowTabService().subscribe(data => {
+      this.dataTitleWorkflowTabService = data;
+    });
+  }
+  // get data Workflow tab service
+  dataWorkflowTabService: PrivateTourResDTO[] = [];
+  getDataTitleWorkflowTabService() {
+    this.appService.getAllDataTitleWorkflowTabService().subscribe(data => {
+      this.dataWorkflowTabService = data;
+    });
+  }
+
+  // ================================================ SUBMIT FORM ============================================== //
+
   private fb = inject(FormBuilder);
   validateForm = this.fb.group({
     name: ['', [Validators.required]],
     email: [''],
     phone: ['', [Validators.required]],
-    note: [''],
+    message: [''],
   });
   submitForm() {
     if (this.validateForm.valid) {
-      console.log('Dữ liệu gửi:', this.validateForm.value);
+      const formData = this.validateForm.getRawValue() as ContactVisaReqDTO;
+      this.appService.createDataContactVisa(formData).subscribe({
+        next: res => {
+          console.log('API Response:', res);
+          this.validateForm.reset();
+        },
+        error: err => {
+          console.error('API Error:', err);
+        },
+      });
     } else {
       Object.values(this.validateForm.controls).forEach(control => {
         if (control.invalid) {
@@ -48,9 +108,5 @@ export class TabServiceComponent {
         }
       });
     }
-  }
-  resetForm(e: MouseEvent): void {
-    e.preventDefault();
-    this.validateForm.reset();
   }
 }
