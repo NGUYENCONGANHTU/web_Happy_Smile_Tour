701f6e169d592ef2eb850530bd1527c5b867b867
RoBin <108477315+NGUYENCONGANHTU@users.noreply.github.com>
2025-07-18 09:04:36 +0700

    update data layout-landing-page

--- src/app/features/layout-landing/layout-landing.component.ts
+++ src/app/features/layout-landing/layout-landing.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import {
+  NavigationEnd,
+  Router,
+  RouterLink,
+  RouterLinkActive,
+  RouterOutlet,
+} from '@angular/router';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 import { NzMenuDirective, NzMenuItemComponent } from 'ng-zorro-antd/menu';
 import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { AppService } from '../../../app.service';
+import { VisaServiceResDTO } from '../../../interface';
+import { NgClass } from '@angular/common';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-layout-landing',
@@ -16,25 +26,41 @@ import { FaIconComponent } from '@fortawesome/angular-fontawesome';
     NzMenuItemComponent,
     FaIconComponent,
     RouterLinkActive,
+    NgClass,
   ],
   templateUrl: './layout-landing.component.html',
   styleUrl: './layout-landing.component.scss',
   standalone: true,
 })
-export class LayoutLandingComponent {
+export class LayoutLandingComponent implements OnInit {
   faCaretDown = faCaretDown;
   languages = [
     { code: 'vi', name: 'Tiếng Việt', flag: 'vn' },
     { code: 'en', name: 'English', flag: 'gb' },
     { code: 'cn', name: '中文', flag: 'cn' },
-    { code: 'vi', name: 'Tiếng Việt', flag: 'vn' },
-    { code: 'en', name: 'English (Philippines)', flag: 'gb' },
-    { code: 'cn', name: '中文', flag: 'cn' },
-    { code: 'vi', name: 'Tiếng Việt', flag: 'vn' },
-    { code: 'en', name: 'English (Hong Kong, SAR)', flag: 'gb' },
-    { code: 'cn', name: '中文', flag: 'cn' },
-    { code: 'vi', name: 'Tiếng Việt', flag: 'vn' },
-    { code: 'en', name: 'English', flag: 'gb' },
-    { code: 'cn', name: '中文', flag: 'cn' },
   ];
+
+  isServiceActive = false;
+
+  selectedServiceId: number | null = null;
+  selectService(id: number) {
+    this.selectedServiceId = id;
+  }
+
+  router = inject(Router);
+  ngOnInit() {
+    this.getDataVisaMenu();
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.isServiceActive = event.url.startsWith('/tab-service');
+      });
+  }
+  appService = inject(AppService);
+  dataVisaMenu: VisaServiceResDTO[] = [];
+  getDataVisaMenu() {
+    this.appService.getAllDataMenuService().subscribe(res => {
+      this.dataVisaMenu = res.data;
+    });
+  }
 }
