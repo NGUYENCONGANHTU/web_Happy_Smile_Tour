c65ea696afd5033a3480f84b757359dccc685d64
RoBin <108477315+NGUYENCONGANHTU@users.noreply.github.com>
2025-08-05 14:16:59 +0700

    update code whatsapp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ChatBoxComponent } from './features/chat-box/chat-box.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ChatBoxComponent],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.scss',
