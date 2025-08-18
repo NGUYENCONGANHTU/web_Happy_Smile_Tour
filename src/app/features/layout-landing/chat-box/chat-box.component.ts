a2a6d86bc5eb20dba2233874c6c74c51206d46a9
RoBin <108477315+NGUYENCONGANHTU@users.noreply.github.com>
2025-08-18 11:21:09 +0700

    update color chat box

--- src/app/features/layout-landing/chat-box/chat-box.component.ts
+++ src/app/features/layout-landing/chat-box/chat-box.component.ts
@@ -2,7 +2,7 @@ import { Component, ElementRef, HostListener, inject } from '@angular/core';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzPopoverModule } from 'ng-zorro-antd/popover';
 import { TranslatePipe } from '@ngx-translate/core';
-
+import { NzMessageService } from 'ng-zorro-antd/message';
 @Component({
   selector: 'app-chat-box',
   standalone: true,
@@ -12,8 +12,8 @@ import { TranslatePipe } from '@ngx-translate/core';
 })
 export class ChatBoxComponent {
   visible = false;
-
   eRef = inject(ElementRef);
+  message = inject(NzMessageService);
   togglePopover(event: MouseEvent): void {
     event.stopPropagation();
     this.visible = !this.visible;
@@ -23,6 +23,25 @@ export class ChatBoxComponent {
   onDocumentClick(event: MouseEvent) {
     if (!this.eRef.nativeElement.contains(event.target)) {
       this.visible = false;
+    }
+  }
+
+  handleCallOrCopy(phone: string) {
+    const isMobile = /Mobi|Android|iPhone/i.test(navigator.userAgent);
+
+    if (isMobile) {
+      // 📱 Nếu là điện thoại -> mở gọi ngay
+      window.location.href = `tel:${phone}`;
+    } else {
+      // 💻 Nếu là desktop -> copy số
+      navigator.clipboard
+        .writeText(phone)
+        .then(() => {
+          this.message.success('Đã sao chép số: ' + phone);
+        })
+        .catch(() => {
+          this.message.error('❌ Sao chép thất bại, vui lòng thử lại!');
+        });
     }
   }
 }
