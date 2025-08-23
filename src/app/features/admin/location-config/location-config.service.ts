22721de2a8eb4877270cecfbcb92d15ac56c2044
HuyPhan <huy.pq@gmail.com>
2025-08-23 14:34:34 +0700

    fix: location config + update api url

--- src/app/features/admin/location-config/location-config.service.ts
+++ src/app/features/admin/location-config/location-config.service.ts
@@ -1,10 +1,14 @@
 import { inject, Injectable } from '@angular/core';
-import {HttpClient, HttpParams} from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from '../../../../environment';
-import {ResponseBaseList} from '../../../core/interfaces/base.interface';
-import {LocationReqDTO, LocationResDTO, LocationTransReqDTO} from './location-config.interface';
+import { ResponseBaseList } from '../../../core/interfaces/base.interface';
+import {
+  LocationReqDTO,
+  LocationResDTO,
+  LocationTransReqDTO,
+} from './location-config.interface';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class LocationConfigService {
   httpClient = inject(HttpClient);
 
@@ -12,7 +16,7 @@ export class LocationConfigService {
   apiUrlTrans = `${environment.API_URL}/location-trans`;
 
   getLocations() {
-    return this.httpClient.get<ResponseBaseList<LocationResDTO>>(this.apiUrl)
+    return this.httpClient.get<ResponseBaseList<LocationResDTO>>(this.apiUrl);
   }
 
   createLocation(data: LocationReqDTO) {
@@ -20,16 +24,19 @@ export class LocationConfigService {
   }
 
   updateLocationById(id: string | number, data: LocationReqDTO) {
-    return this.httpClient.put(this.apiUrl + "/" + id, data);
+    return this.httpClient.put(this.apiUrl + '/' + id, data);
   }
 
   deleteLocationById(id: string | number) {
-    return this.httpClient.delete(this.apiUrl + "/" + id);
+    return this.httpClient.delete(this.apiUrl + '/' + id);
   }
 
   getLocationsTrans(langCode: string) {
     const params = new HttpParams().set('langCode', langCode);
-    return this.httpClient.get<ResponseBaseList<LocationResDTO>>(this.apiUrlTrans, {params})
+    return this.httpClient.get<ResponseBaseList<LocationResDTO>>(
+      this.apiUrlTrans + '/all',
+      { params }
+    );
   }
 
   createLocationTrans(data: LocationTransReqDTO) {
@@ -37,7 +44,6 @@ export class LocationConfigService {
   }
 
   updateLocationTransById(id: string | number, data: LocationTransReqDTO) {
-    return this.httpClient.put(this.apiUrlTrans + "/" + id, data);
+    return this.httpClient.put(this.apiUrlTrans + '/' + id, data);
   }
-
 }
